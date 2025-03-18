7763ccaf5463e2dda40e310fa67f29f3ab0f82f7
Victor Smith <me@victorsmith.dev>
2025-03-18 09:56:26 -0600

    start code

---
+++ src/the-button/the-button.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-the-button',
+  imports: [],
+  templateUrl: './the-button.component.html',
+  styleUrl: './the-button.component.scss',
+})
+export class TheButtonComponent {
+  // TODO:
+  // 1) Add a button to the component
+  // https://material.angular.io/components/button/overview
+  // 2) Style the button - use the angular styling API
+  // https://material.angular.io/components/button/styling
+  // a) Change the background colour to Green
+  // b) Change the text size to 14px
+  // c) Modify text spacing
+}
